37f06e39b45a37355fbf9ec105bb089966ca7c7a
dina0abdo <dinaabdo386@gmail.com>
2023-02-04 17:37:32 +0200

    sec push

--- shops/urls.py
+++ shops/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.shops, name='shops'),
-    path('product_single', views.product_single, name='product_single'),
+    path('product_single', views.product_single, name='product_single')
 ]
